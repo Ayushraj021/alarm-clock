10bc65fb059337fb6b8a24dbed8bf56775b4d894
Ayushraj021 <ayushraj02929@gmail.com>
2024-09-01 18:41:12 +0530

    adding alarm clock

---
+++ alarm clock.py
@@ -0,0 +1,67 @@
+from tkinter.ttk import *
+from tkinter import *
+
+from PIL import ImageTk, Image
+
+#color
+bg_color = '#ffffff'
+col1 = 'black'
+col2 = 'blue'
+
+
+#window
+window = Tk()
+window.title("")
+window.geometry('300x150')
+window.config(bg=bg_color)
+
+#frame up
+
+frame_line = Frame(window, width=400, height=2, bg=col1)
+frame_line.grid(row=0, column=0)
+
+frame_body = Frame(window, width=400, height=290, bg=bg_color)
+frame_body.grid(row=1, column=0)
+
+#configuring frame body
+
+img = Image.open('image.png')
+img.resize((100,100))
+img = ImageTk.PhotoImage(img)
+
+app_image =Label(frame_body, height=100, image=img, bg= bg_color)
+app_image.place(x=10, y=10)
+
+name = Label(frame_body, text= 'Alarm', height=1, font=('Ivy 18 bold'), bg=bg_color)
+name.place(x=125, y=10)
+
+hours = Label(frame_body, text= 'Hours', height=1, font=('Ivy 10 bold'), fg=col2, bg= bg_color)
+hours.place(x=127, y=40)
+c_hours = Combobox(frame_body, width=2, font=('arial 15'))
+c_hours['values']= ("00","01","02","03","04","05","06","07","08","09","10","11","12", "13","14","15","16","17","18","19","20","21","22","23")
+c_hours.current(0)
+c_hours.place(x=130, y=58)
+
+
+minutes = Label(frame_body, text= 'Mins', height=1, font=('Ivy 10 bold'), fg=col2, bg= bg_color)
+minutes.place(x=177, y=40)
+c_minutes = Combobox(frame_body, width=2, font=('arial 15'))
+c_minutes['values']= ("00","01","02","03","04","05","06","07","08","09","10","11","12","13","14","15","16","17","18","19","20","21","22","23","24","25","26","27","28","29","30","31","32","33","34","35","36","37","38","39","40","41","42","43","44","45","46","47","48","49","50","51","52","53","54","55","56","57","58","59")
+c_minutes.current(0)
+c_minutes.place(x=180, y=58)
+
+
+
+seconds = Label(frame_body, text= 'Secs', height=1, font=('Ivy 10 bold'), fg=col2, bg= bg_color)
+seconds.place(x=227, y=40)
+c_seconds = Combobox(frame_body, width=2, font=('arial 15'))
+c_seconds['values']= ("00","01","02","03","04","05","06","07","08","09","10","11","12","13","14","15","16","17","18","19","20","21","22","23","24","25","26","27","28","29","30","31","32","33","34","35","36","37","38","39","40","41","42","43","44","45","46","47","48","49","50","51","52","53","54","55","56","57","58","59")
+c_seconds.current(0)
+c_seconds.place(x=230, y=58)
+
+selected = IntVar()
+
+rad1 = Radiobutton(frame_body, font=('arial 10 bold'), value=1, text="Activate", bg=bg_color)
+rad1.place(x=125, y=95)
+
+window.mainloop()
---
+++ demo.py
@@ -0,0 +1,11 @@
+from tkinter.ttk import *
+from tkinter import *
+from pygame import mixer
+
+
+#window
+window = Tk()
+window.title("")
+window.geometry('300x150')
+
+window.mainloop()
